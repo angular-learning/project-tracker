ba67f4dc570be6f5694cc55bb23fec3ee1cb4a5f
VanDalkvist <i_van@list.ru>
2016-08-26 13:11:11 +0300

    fixed path

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // set up ======================================================================
 var express  = require('express');
+var path = require('path');
+
 var app      = express(); 								// create our app w/ express
 var mongoose = require('mongoose'); 					// mongoose for mongodb
 var port  	 = process.env.PORT || 8080; 				// set the port
@@ -14,7 +16,7 @@ mongoose.connect(database.url, function (err) {
 }); 	// connect to mongoDB database on modulus.io
 
 app.configure(function() {
-	app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+	app.use(express.static(path.join(__dirname, 'public'))); 		// set the static files location /public/img will be /img for users
 	app.use(express.logger('dev')); 						// log every request to the console
 	app.use(express.bodyParser()); 							// pull information from html in POST
 	app.use(express.methodOverride()); 						// simulate DELETE and PUT
