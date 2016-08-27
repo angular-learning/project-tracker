86cc0619a39cf037f6bb56da1dbd2612e27e2770
Ivan <i_van@list.ru>
2016-08-28 00:46:46 +0300

    upgrade express; added "bower i" command to pre start command; (#3)

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 module.exports = {
-
-  // the database url to connect
-  url: process.env.MONGODB_URI || 'mongodb://localhost:27017/project-tracker'
+    // the database url to connect
+    url: process.env.MONGODB_URI || 'mongodb://localhost:27017/project-tracker'
 };
