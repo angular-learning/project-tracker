82f6997192c60cfbbee77285ced7ace6510413bb
Dmitry Golitsyn <dmitry.golitsyn@firstlinesoftware.com>
2016-08-31 19:35:33 +0300

    Bugfix

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -31,7 +31,7 @@ angular
                     self.newTodo = {};
                 })
                 .finally(function() {
-                    //self.loading = false;
+                    self.loading = false;
                 });
         }
 
