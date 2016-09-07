1fbf651c1c07f744fb88763e1516218a237d7654
Дмитрий <golitsyn.dmitry@gmail.com>
2016-09-08 00:51:38 +0300

    Added bootstrap and ui-router

---
+++ public/js/ui.router.module.js
@@ -0,0 +1,57 @@
+angular.module('uiRouterModule', ['ui.router'])
+    .run(
+    ['$rootScope', '$state', '$stateParams',
+        function ($rootScope, $state, $stateParams) {
+
+            // It's very handy to add references to $state and $stateParams to the $rootScope
+            // so that you can access them from any scope within your applications.For example,
+            // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
+            // to active whenever 'contacts.list' or one of its decendents is active.
+            $rootScope.$state = $state;
+            $rootScope.$stateParams = $stateParams;
+        }
+    ])
+    .config(['$stateProvider', '$urlRouterProvider',
+        function ($stateProvider, $urlRouterProvider) {
+
+            /////////////////////////////
+            // Redirects and Otherwise //
+            /////////////////////////////
+
+            // Use $urlRouterProvider to configure any redirects (when) and invalid urls (otherwise).
+            $urlRouterProvider
+
+                // The `when` method says if the url is ever the 1st param, then redirect to the 2nd param
+                // Here we are just setting up some convenience urls.
+                .when('/tasks', '/tasks')
+
+                // If the url is ever invalid, e.g. '/asdf', then redirect to '/' aka the home state
+                .otherwise('/');
+
+
+            //////////////////////////
+            // State Configurations //
+            //////////////////////////
+
+            // Use $stateProvider to configure your states.
+            $stateProvider
+
+                //////////
+                // Main //
+                //////////
+
+                .state("main", {
+                    url: "/",
+                    template: '<h1>Welcome to the project tracker</h1>'
+                })
+
+                ///////////
+                // Tasks //
+                ///////////
+
+                .state('tasks', {
+                    url: '/tasks',
+                    templateUrl: '../templates/tasks.tmpl.html'
+                })
+        }
+    ]);
