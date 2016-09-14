10b72422349cd086bcf172c9e3034792ff6adf9c
Дмитрий <golitsyn.dmitry@gmail.com>
2016-09-15 01:47:45 +0300

    Created separate routes file for tasks

--- public/app/core/pt.config.js
+++ public/app/core/pt.config.js
@@ -2,33 +2,16 @@
     angular
         .module('projectTracker')
         .run(['$rootScope', '$state', '$stateParams', _run])
-        .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', _config]);
+        .config(['$urlRouterProvider', '$locationProvider', _config]);
 
     function _run(rootScope, state, stateParams) {
         rootScope.$state = state;
         rootScope.$stateParams = stateParams;
     }
 
-    function _config(stateProvider, urlRouterProvider, locationProvider) {
+    function _config(urlRouterProvider, locationProvider) {
         urlRouterProvider
             .otherwise('/list');
-
-        stateProvider
-            .state("tasks", {
-                abstract: true,
-                url: '/',
-                controller: 'mainController as mainCtrl',
-                template: '<div ui-view></div>'
-            })
-            .state('tasks.list', {
-                url: 'list',
-                controller: 'taskController as taskCtrl',
-                templateUrl: '/app/tasks/tasks-list.view.tmpl.html'
-            })
-            .state('tasks.details', {
-                url: 'details',
-                templateUrl: '/app/tasks/tasks-details.view.tmpl.html'
-            });
 
         locationProvider.html5Mode(true);
     }
