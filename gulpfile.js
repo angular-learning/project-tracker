e38af5b192ef47a2d2a1589178c7d5b780645e6a
VanDalkvist <i_van@list.ru>
2016-09-15 22:49:12 +0300

    added npm scripts; added livereload;

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+// dependencies
+
+var fs = require('fs');
+
+var tasks = fs.readdirSync('./build');
+
+tasks.forEach(function (taskName) {
+    var modulePath = './build/' + taskName;
+
+    if (fs.lstatSync(modulePath).isDirectory()) return;
+
+    require(modulePath);
+});
