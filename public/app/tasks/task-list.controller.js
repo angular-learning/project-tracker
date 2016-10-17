b1620f48dd0d55583d4e892ba11c67f6e549e193
Дмитрий <golitsyn.dmitry@gmail.com>
2016-10-18 02:15:41 +0300

    First attempt to use lunr

--- public/app/tasks/task-list.controller.js
+++ public/app/tasks/task-list.controller.js
@@ -13,15 +13,31 @@
         self.initializing = true;
         self.selectedId = $stateParams.id;
 
-        Task.query(function (data) {
-            self.tasks = data;
-            self.initializing = false;
-        });
+        Task.query(function (data) { self.searchIndex = _initSearchIndex(data); })
+            .$promise.finally(function () {
+            if ($stateParams.search) {
+                var qq = self.searchIndex.search($stateParams.search);
+                var tt = self.searchIndex.search($stateParams.search)
+                        .map(function(doc) { return doc.ref; });
+                Task.query({
+                    id: {$in: tt}
+                }, 
+                function (data) { _initList(data); });
+            }
+            else {
+                Task.query(function (data) { _initList(data); });   
+            } 
+        });    
 
         self.createTask = _createTask;
         self.updateTask = _updateTask;
         self.deleteTask = _deleteTask;
         self.selectTask = _selectTask;
+
+        function _initList(data) {
+            self.tasks = data;
+            self.initializing = false;
+        }
 
         function _createTask() {
             if (!self.newTask.title)
@@ -29,9 +45,10 @@
 
             self.loading = true;
 
-            return Task.save(self.newTask, function (data) {
-                self.tasks.push(data);
+            return Task.save(self.newTask, function (task) {
+                self.tasks.push(task);
                 toastr.info('Task ' + self.newTask.title + ' created!');
+                self.searchIndex.add(task);
                 self.newTask = {};
             }).$promise.finally(function () {
                 self.loading = false;
@@ -43,6 +60,7 @@
             self.loading = true;
 
             return Task.save({ id: task.id }, task).$promise.finally(function () {
+                self.searchIndex.update(task);
                 self.loading = false;
             });
         }
@@ -52,6 +70,7 @@
 
             Task.delete({ id: task.id }, function (data) {
                 _.remove(self.tasks, { id: task.id });
+                self.searchIndex.remove(task);
             }).$promise.finally(function () {
                 self.loading = false;
             });
@@ -66,6 +85,19 @@
                 self.selectedId = undefined;
                 $state.go('layout.tasks');
             }
+        }
+
+        function _initSearchIndex(data){
+            var searchIndex = lunr(function () {
+                this.field('title');
+                this.ref('id');
+            });
+
+            for (var item in data) {
+                searchIndex.add(data[item]);
+            }
+
+            return searchIndex;
         }
     }
 })();
