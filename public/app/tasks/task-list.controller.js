19976e982572bfa5ab3072276c2f1bb73b1412f3
Dmitry Golitsyn <dmitry.golitsyn@firstlinesoftware.com>
2016-12-13 19:28:09 +0300

    Search reinitialized when new tasks added

--- public/app/tasks/task-list.controller.js
+++ public/app/tasks/task-list.controller.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
 
     _controller.$inject = ['TaskService', 'toastr', '$state', '$stateParams'];
 
@@ -17,25 +17,24 @@
         self.newTask = {};
         self.initializing = true;
 
-        _initList();
+        _initList(true);
 
-        function _initSearch(data) {
-            var foundIds = self.searchIndex.search($stateParams.search)
+        function _initSearch(data, query) {
+            if (!query) return data;
+
+            var foundIds = self.searchIndex.search(query)
                 .map(function(doc) { return doc.ref; });
 
             return _(data).keyBy('id').at(foundIds).filter().value();
         }
 
-        function _initList() {
+        function _initList(needInitSearchIndex) {
             TaskService.getList().then(function(data) {
-                self.searchIndex = _initSearchIndex(data);
-                if ($stateParams.search) {
-                    self.tasks = _initSearch(data);                    
-                }
-                else {                                
-                    self.tasks = data;
-                }
 
+                if (needInitSearchIndex)
+                    self.searchIndex = _initSearchIndex(data);
+                
+                self.tasks = _initSearch(data, $stateParams.search);
                 _selectTask(_.find(self.tasks, { id: $stateParams.id }));
 
                 self.initializing = false;
@@ -49,16 +48,13 @@
             self.loading = true;
 
             return TaskService.create(self.newTask)
-                .then(function (task) {
-                    self.selectedId = $stateParams.id;
-                    TaskService.getList().then(function (data) {
-                        self.tasks = data;
-                    });
+                .then(function(task) {
+                    _initList(false);
                     toastr.info('Task ' + self.newTask.title + ' created!');
                     self.searchIndex.add(task);
                     self.newTask = {};
                 })
-                .finally(function () {
+                .finally(function() {
                     self.loading = false;
                 });
         }
@@ -66,7 +62,7 @@
         function _updateTask(task) {
             self.loading = true;
             return TaskService.update(task)
-                .finally(function () {
+                .finally(function() {
                     self.searchIndex.update(task);
                     self.loading = false;
                 });
@@ -76,7 +72,7 @@
             self.loading = true;
 
             TaskService.delete(task.id)
-                .then(function (data) {
+                .then(function(data) {
                     _.remove(self.tasks, { id: task.id });
                     self.searchIndex.remove(task);
 
@@ -84,13 +80,13 @@
                         _selectTask(undefined);
                     }
                 })
-                .finally(function () {
+                .finally(function() {
                     self.loading = false;
                 });
         }
 
         function _selectTask(task) {
-            if (task) {                
+            if (task) {
                 self.selectedId = task.id;
                 $state.go('details', { id: task.id }, { reload: 'details' });
             }
@@ -101,7 +97,7 @@
         }
 
         function _initSearchIndex(data) {
-            var searchIndex = lunr(function () {
+            var searchIndex = lunr(function() {
                 this.field('title', { boost: 50 });
                 this.ref('id');
             });
