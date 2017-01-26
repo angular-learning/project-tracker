c89fc1450ef2003d0bc1fabed746a5a836d5e51c
Дмитрий <golitsyn.dmitry@gmail.com>
2017-01-27 00:27:47 +0300

    Changed node api folder structure

--- app/modules/history/models/history.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-
-module.exports = mongoose.model('History', {    
-    description: String,
-    modifiedAt: Date,
-    modifiedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-});
